37c33ef71b025915241660331318350f8b9ce2eb
luqman Shadow <luqmankyembe17@gmail.com>
2024-09-11 22:31:11 +0300

    trail_3

--- trail_3.py
+++ trail_3.py
@@ -1,2 +1,21 @@
 # more on method 
 # starting with 2
+class Fruits:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def show_info(self):
+        print(f"Fruit: {self.name}, Color: {self.color}")
+
+    def is_ripe(self):
+        ripe_fruits = ["banana", "apple", "mango"]
+        return self.name.lower() in ripe_fruits
+
+
+fruit1 = Fruits("Apple", "Red")
+fruit1.show_info()
+print(f"Is the fruit ripe? {fruit1.is_ripe()}")
+
+
+# 3 methods
