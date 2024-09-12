7c300d8875eaa5b8e312974f11c3b248cac7cd40
luqman Shadow <luqmankyembe17@gmail.com>
2024-09-12 05:14:49 +0300

    changes

--- assigent_classes/studentGrade.py
+++ assigent_classes/studentGrade.py
@@ -9,6 +9,6 @@ class Student:
             return 0.0
         return sum(self.grades) / len(self.grades)
 
-# Example usage
+
 student = Student("Alice", 16, [85, 90, 78, 92])
 print(f"Name: {student.name}, Age: {student.age}, GPA: {student.calculate_gpa():.2f}")
