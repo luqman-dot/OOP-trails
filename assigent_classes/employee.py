bb7c96248aea0d5710f96a9a0f0b32ff8e0a2a2d
luqman Shadow <luqmankyembe17@gmail.com>
2024-09-12 08:27:09 +0300

    employee

---
+++ assigent_classes/employee.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, name, emp_id, department, salary):
+        self.name = name
+        self.emp_id = emp_id
+        self.department = department
+        self.salary = salary
+
+    def display_info(self):
+        print(f"Employee Name: {self.name}")
+        print(f"Employee ID: {self.emp_id}")
+        print(f"Department: {self.department}")
+        print(f"Salary: ${self.salary}")
+
+    def give_raise(self, amount):
+        self.salary += amount
+        print(f"{self.name} has been given a raise of ${amount}. New salary is ${self.salary}.")
+
+
+employee_1 = Employee("Luqman Kyembe", 101, "HR", 50000)
+employee_1.display_info()
+
+employee_1.give_raise(5000)
+employee_1.display_info()
