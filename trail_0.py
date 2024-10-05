046b0993603039a70e5499ce57fdb6022e493ec2
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-05 09:53:04 +0300

    car

--- trail_0.py
+++ trail_0.py
@@ -2,14 +2,16 @@
 class Car: 
     def __init__(self, color, model, numberplate, year):
         
-        # instance variable
-        self.color = color
-        self.model = model
-        self.numberplate = numberplate
+        self.color = color  # instance variable
+        self.model = model  # instance variable
+        self.numberplate = numberplate  # instance variable
         self.year = year
+        
+    def display_info(self):
+        print(f"car: {self.color} {self.model} {self.numberplate} {self.year}")    
 
 car_1 = Car("yellow", "Mustang", 899, 2000)
 car_2 = Car("green", "Lambo", 100, 2020)
 
-print(car_1.model)
+print(car_1.display_info())
 
