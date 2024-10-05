fbb4f7c6c71c6081bd3f01f594787f931ff622b8
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-05 10:31:17 +0300

    car

--- cars/car.py
+++ cars/car.py
@@ -1,11 +1,25 @@
-from car import Car
+class Car:
+    total_cars = 0    #class variable  
 
-car1 = Car("vostro",4300,"red","dell")
-car2 = Car("pro",43,"silver","hp")
-car3 = Car("thinkpad",1,"black","lenovo")
-car4 = Car("yoga",0,"gray","lenovo")
-car5 = Car("macbook",4300,"pink","apple")
+    def __init__(self, color, model , numberplate, year):
+        self.color = color  # instance variable
+        self.model = model  # instance variable
+        self.numberplate = numberplate  # instance variable
+        self.year = year
 
-print(car1.name)
-print(car2.color)
-print(car3.model)
+        Car.total_cars += 1
+
+    def display_info(self):
+        print(f"Car: {self.color} {self.model} {self.numberplate} {self.year}")
+
+    @classmethod
+    def display_total_cars(cls):
+        print(f"Total number of cars: {cls.total_cars}")
+
+car_1 = Car("yellow", "Mustang", 899, 2000)
+car_2 = Car("green", "Lambo", 100, 2020)
+
+car_1.display_info()
+car_2.display_info()
+
+Car.display_total_cars()
