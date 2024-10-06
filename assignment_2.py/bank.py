8cdb22f30a63b56dc69ea9921f99416579cc70f8
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-06 19:18:00 +0300

    comments

--- assignment_2.py/bank.py
+++ assignment_2.py/bank.py
@@ -1,22 +1,22 @@
 class BankAccount:
-    interest_rate = 0.05  
+    interest_rate = 0.05    #class attribute rep the interest rate for all accounts
 
-    def __init__(self, account_holder):
+    def __init__(self, account_holder):    #initialize the accounts
         self.account_holder = account_holder
         self.balance = 0.0 
 
-    def deposit(self, amount):
+    def deposit(self, amount):  #func to add the specified amount to the balance and display the updated balance
         self.balance += amount
         print(f"Deposited {amount} into {self.account_holder}'s account. New balance: {self.balance}")
 
-    def withdraw(self, amount):
+    def withdraw(self, amount):  #check if bank balance is sufficient
         if amount <= self.balance:
             self.balance -= amount
             print(f"Withdrew {amount} from {self.account_holder}'s account. New balance: {self.balance}")
         else:
             print(f"Insufficient funds in {self.account_holder}'s account. Withdrawal of {amount} failed. Current balance: {self.balance}")
 
-    def apply_interest(self):
+    def apply_interest(self):   #calc  the interest based on the balance 
         interest = self.balance * BankAccount.interest_rate
         self.balance += interest
         print(f"Applied interest to {self.account_holder}'s account. Interest added: {interest}. New balance: {self.balance}")
@@ -24,7 +24,7 @@ class BankAccount:
     def display_account_info(self):
         print(f"Account holder: {self.account_holder}, Current Balance: {self.balance}")
 
-account1 = BankAccount("Luqman")
+account1 = BankAccount("Luqman")  #BankAccount objects for different account holders
 account2 = BankAccount("Shadowsman")
 
 # Luqman 
--- assignment_2.py/online_shopping.py
+++ assignment_2.py/online_shopping.py
@@ -1,25 +1,25 @@
 class Product:
-    def __init__(self, product_name, price, available_in_stock):
+    def __init__(self, product_name, price, available_in_stock):  #initializes a product 
         self.product_name = product_name  
         self.price = price  
         self.available_in_stock = available_in_stock  
 
-    def display_product_info(self):
+    def display_product_info(self):  #display product information
         print("Product:", self.product_name)
         print("Price:", self.price)
         print("Stock:", self.available_in_stock)
 
-class ShoppingCart:
+class ShoppingCart:   #initializes an empty list to hold the items added to the cart
     def __init__(self):
         self.items = [] 
 
     # stock checking fun
-    def add_to_cart(self, product, quantity):
-        if product.available_in_stock >= quantity:
+    def add_to_cart(self, product, quantity):  #add specific items and quantity  to the shopping cart
+        if product.available_in_stock >= quantity: # Adds the product and its quantity to the cart if enough stock is available
             self.items.append([product, quantity]) 
-            product.available_in_stock -= quantity  
+            product.available_in_stock -= quantity # Reduces the available stock of the product 
             print(f"Added {quantity} {product.product_name}(s) to the cart.")
-        else:
+        else: # Notifies the user if there is insufficient stock
             print("Not enough stock.")
 
     def display_cart(self):
@@ -28,7 +28,7 @@ class ShoppingCart:
             product, quantity = item
             print(f"{product.product_name}: {quantity} unit(s)")
 
-    def calculate_total(self):
+    def calculate_total(self): #calc total cost in the cart
         total = 0
         for item in self.items:
             product, quantity = item
