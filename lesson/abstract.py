fdef2349ff91e1bfcd922839091ccb6c40ae13ce
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-10 11:00:01 +0300

    abstract

--- lesson/abstract.py
+++ lesson/abstract.py
@@ -15,6 +15,15 @@ class Car(Vehicle):
     def stop(self):
         print("you can stop")
         
-car1 = Car()
-car1.go() 
+# car1 = Car()
+# car1.go() 
+
+class Boat(Vehicle):
+    def go(self):
+        print(f"you sail the boat")
+        
+    def stop(self):
+        print(f"you stop the boat")
     
+boat=Boat()
+boat.go() 
