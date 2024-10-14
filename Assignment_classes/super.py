42f29667e55903a50162045778aa0ea453f331dc
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-14 08:51:38 +0300

    super

---
+++ Assignment_classes/super.py
@@ -0,0 +1,21 @@
+class Shape():
+    def __init__(self, color, name):
+        self.color = color
+        self.is_filled = is_filled
+
+def describe(self):
+    print(f"if is {self.color} and {'filled'if self.is_filled else 'not filled'}")
+    
+class Circle(Shape):
+    def __init__(self, color, is_filled.radius):
+        super(). __init__(color, is_filled)
+        self.radius = radius
+        
+class Square(Shape):
+        def __init__(self, color, is_filled.radius):
+            super(). __init__(color, is_filled)
+            self.width = width
+            
+describe(Circle)
+    
+    
