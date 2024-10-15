458c9ba692758fcb1cc7c9ff913978146972b80d
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-15 08:17:29 +0300

    painting

--- Assignment_classes/super.py
+++ Assignment_classes/super.py
@@ -3,8 +3,8 @@ class Shape():
         self.color = color
         self.is_filled = is_filled
 
-def describe(self):
-    print(f"if is {self.color} and {'filled'if self.is_filled else 'not filled'}")
+    def describe(self):
+        print(f"it is {self.color} and {'filled'if self.is_filled else 'not filled'}")
     
 class Circle(Shape):
     def __init__(self, color, is_filled,radius):
@@ -17,5 +17,5 @@ class Square(Shape):
             self.width = width
             
 circle = Circle(color="red", is_filled = "True" , radius = "3cm")
-print(circle.radius)
-# circle.describe()
+# print(circle.radius)
+circle.describe()
