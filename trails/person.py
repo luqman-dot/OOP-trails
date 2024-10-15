51fae3c472d3ee8afcc067bfbd6dfe8c015a64b5
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-15 09:44:04 +0300

    modified:   trails/person.py

--- trails/person.py
+++ trails/person.py
@@ -1,25 +1,34 @@
 class Person:
-    def __init__(self,name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
         
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
         
-# person1 = Person("luqman", 25)
+# person1 = Person(name= "luqman", age= "20")
 # person1.display_info()
+
 class Student(Person):
     def __init__(self, name, age, grade):
         super().__init__(name, age)
         self.grade = grade
+    
+    def display_info(self):
+        super().display_info()
+        print(f"Grade: {self.grade}")
         
 class Lecturer(Person):
     def __init__(self, name, age, department):
         super().__init__(name, age)
         self.department = department
-        
-student = Student(name = "luqman",age = "20",grade = "G1")
-lecturer = Lecturer(name = "Bosco", age= "20", department="computing")
+    
+    def display_info(self):
+        super().display_info()
+        print(f"Department: {self.department}")
+
+student = Student(name="Luqman", age=20, grade="G1")
+lecturer = Lecturer(name="Bosco", age=20, department="Computing")
+
 lecturer.display_info()
 student.display_info()
-        
