193fc418a8dce7146f57530ccc281a32c3ca9c77
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-15 09:35:30 +0300

    equal

--- trails/person.py
+++ trails/person.py
@@ -8,10 +8,18 @@ class Person:
         
 # person1 = Person("luqman", 25)
 # person1.display_info()
-
 class Student(Person):
     def __init__(self, name, age, grade):
-        print(f"{self.name} {self.age} {self.grade}")
-
-
-                
+        super().__init__(name, age)
+        self.grade = grade
+        
+class Lecturer(Person):
+    def __init__(self, name, age, department):
+        super().__init__(name, age)
+        self.department = department
+        
+student = Student(name = "luqman",age = "20",grade = "G1")
+lecturer = Lecturer(name = "Bosco", age= "20", department="computing")
+lecturer.display_info()
+student.display_info()
+        
