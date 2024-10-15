ac94a9c4cd25caac7fedc48271b0bd164a0e106c
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-15 08:58:03 +0300

    git

--- trails/person.py
+++ trails/person.py
@@ -6,10 +6,12 @@ class Person:
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
         
-person1 = Person("luqman", 25)
-person1.display_info()
-person2 = Person("kyembe", 30)
-person2.display_info()
-person3 = Person("Bob", 40)
-person3.display_info()
-        
+# person1 = Person("luqman", 25)
+# person1.display_info()
+
+class Student(Person):
+    def __init__(self, name, age, grade):
+        print(f"{self.name} {self.age} {self.grade}")
+
+
+                
