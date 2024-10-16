1addbf7974a9412171c5090065c8a6fa58305da9
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-16 16:54:17 +0300

    new file:   Assignment_classes/patient.py

---
+++ Assignment_classes/patient.py
@@ -0,0 +1,8 @@
+class Patient:
+    def __init__(self, name , patient_ID, room , ):
+        self.name=name
+        self.patient_ID=patient_ID
+        self.room=room
+        
+    def display_info(self):
+        
