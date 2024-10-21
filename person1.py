3721db34ad63200b724d5f52b62ca8c3d3e30172
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-21 11:08:44 +0300

    new file:   person1.py

---
+++ person1.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__ (mysillyproject, name , age):
+        mysillyproject.name = name
+        mysillyproject.age = age
+        
+    def my_name(ABC):
+        print("hello world my name is + ABC.name")
+    
+    
+P1= ("John", 20)
+P1.my_name()
