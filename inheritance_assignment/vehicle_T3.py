934e88591d7bb8c40db2c63aa652cf3d5343d6e1
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-29 13:58:17 +0300

    modified:   inheritance_assignment/vehicle_T3.py

---
+++ inheritance_assignment/vehicle_T3.py
@@ -0,0 +1,24 @@
+class Vehicle:
+    def __init__(self, model):
+        self.model = model
+        
+    def race(self):
+        print("Start your engine!!!!")
+
+class Car(Vehicle):
+    def __init__(self, model, number_of_doors):
+        super().__init__(model)
+        self.number_of_doors = number_of_doors
+
+class Ferrari(Car):
+    def __init__(self, model, number_of_doors, battery_capacity):
+        super().__init__(model, number_of_doors)
+        self.battery_capacity = battery_capacity
+
+    def show_details(self):
+        print(f"Type: {self.model}, Doors: {self.number_of_doors}, Battery: {self.battery_capacity}kWh")
+        
+car = Ferrari("ferrari california", 2, 75)
+car.race()
+car.show_details()
+ 
