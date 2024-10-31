a3c7f93473c34a0e09df9c4578675fddb360ec40
luqman Shadow <luqmankyembe17@gmail.com>
2024-10-31 14:25:08 +0300

    modified:   trails/football.py

--- trails/football.py
+++ trails/football.py
@@ -1,10 +1,23 @@
 class Football:
-    def __init__(self, name , TeamColor):
+    def __init__(self, name , TeamColor, sologun):
         self.name= name
         self.TeamColor= TeamColor
+        self.sologun= sologun
     
     def show_info(self):
-        return f"name:{self.name}/n color:{self.TeamColor}"
+        print(f"Team name:{self.name}\nSologun:{self.sologun}\nTeam Color:{self.TeamColor}")
     
-class Sologun(Football):
+class Uniform(Football):
+    def __init__(self, name, TeamColor, sologun, size, weight, material):
+        super().__init__(name, TeamColor, sologun)
+        self.size= size
+        self.weight= weight
+        self.material= material
+        
+    def show_info(self):
+        super().show_info()
+        print(f"Size:{self.size}, Weight:{self.weight}, Material:{self.material}")
+            
+team1 = Uniform("Chelsea", "blue & white", "The pride of london", 5, 450, "Poly")
+team1.show_info()
     
