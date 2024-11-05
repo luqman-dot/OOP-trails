e74daa638496718e58f783835a08c93f4287b94d
luqman Shadow <luqmankyembe17@gmail.com>
2024-11-05 09:48:20 +0300

    new file:   tesla.py

---
+++ tesla.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__ (self, brand ,model, year):
+        self.brand= brand
+        self._model= model
+        self.__year= year 
+        
+    def get_brand(self):
+        return self.brand
+    def get_model(self):
+        return self._model
+    def get__year(self):
+        return self.__year
+    def set__year(self, year):
+        self.__year= year
+        
+car1= Car("ferrari ", "california", 2002)
+car1.set__year(2024)
+print(car1.brand)
+print(car1._model)
+print(car1.get__year())
